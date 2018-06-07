bc13897a5469549c864ccfc8774f8acb6bb09296
HUQAFU <jokebop@gmail.com>
2018-06-07 19:30:24 +0300

    Effect on Ball tap added

---
+++ Assets/Effect.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Dumme Effect Manager
+[RequireComponent(typeof(Ball))]
+public class Effect : MonoBehaviour {
+    [SerializeField]
+    GameObject _popEffectPrefab;
+    Ball _ball;
+
+    private void Awake()
+    {
+        _ball = GetComponent<Ball>();
+    }
+
+    void OnEnable()
+    {
+        _ball.BallTaped += OnBallTaped;
+    }
+
+    void OnDisable()
+    {
+        _ball.BallTaped -= OnBallTaped;
+    }
+
+    void OnBallTaped(GameObject ball)
+    {
+        if (_popEffectPrefab) {
+            Instantiate(_popEffectPrefab, ball.transform.position, ball.transform.rotation);
+        }
+    }
+}
