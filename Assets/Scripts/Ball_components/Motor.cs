1bd5ee2d4c21cd51fbe25f186af1dd4e4380d742
HUQAFU <jokebop@gmail.com>
2018-06-07 17:32:01 +0300

    Code description added

--- Assets/Scripts/Ball_components/Motor.cs
+++ Assets/Scripts/Ball_components/Motor.cs
@@ -1,16 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/// <summary>
+/// Moves object by Speed to _direction
+/// Author: Adik
+/// </summary>
 public class Motor : MonoBehaviour
 {
+    /// <summary>
+    /// Movement speed
+    /// </summary>
     public float Speed = 0;
 
+    /// <summary>
+    /// Direction of movement
+    /// </summary>
     [SerializeField]
     Vector3 _direction = Vector3.up;
 
-    //Just for pseudo baloon movement
-    [SerializeField, Range(0,1)]
+    /// <summary>
+    /// Movement noize amount - just for pseudo baloon movement
+    /// </summary>
+    [SerializeField, Range(0, 1)]
     float _noizeAmount = 0;
 
     void Update()
@@ -18,6 +29,10 @@ public class Motor : MonoBehaviour
         Move(Speed);
     }
 
+    /// <summary>
+    /// Move object by Speed to _direction
+    /// </summary>
+    /// <param name="speed">Movement speed</param>
     void Move(float speed)
     {
         Vector3 direction = _direction;
